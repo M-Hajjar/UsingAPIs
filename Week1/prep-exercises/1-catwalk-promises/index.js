caad96431799c12fab42afa3e68c79ed38566455
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-06-08 21:21:44 +0200

    first weeek

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -5,22 +5,37 @@ const STEP_INTERVAL_MS = 50;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL =
+  'http://www.anniemation.com/clip_art/images/cat-walk.gif'; // Assuming this is the walking cat URL
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+    let currentPos = startPos;
+    img.style.left = `${currentPos}px`;
+
+    const step = () => {
+      if ((startPos < stopPos && currentPos >= stopPos) || (startPos > stopPos && currentPos <= stopPos)) {
+        resolve();
+      } else {
+        currentPos += (startPos < stopPos) ? STEP_SIZE_PX : -STEP_SIZE_PX;
+        img.style.left = `${currentPos}px`;
+        setTimeout(step, STEP_INTERVAL_MS);
+      }
+    };
+
+    step();
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+    
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -30,11 +45,15 @@ function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  const repeat = async () => {
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos, stopPos);
+    repeat();
+  };
+
+  repeat();
 }
 
 window.addEventListener('load', catWalk);
+;
