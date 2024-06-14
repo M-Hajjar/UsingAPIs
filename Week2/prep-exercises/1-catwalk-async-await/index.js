57f4e60b4ddeb396a5f413b4deaaaa43b58a4bd1
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-06-14 18:40:19 +0200

    Update index.js ( solutions )

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -3,18 +3,35 @@
 const STEP_INTERVAL_MS = 50;
 const STEP_SIZE_PX = 10;
 const DANCE_TIME_MS = 5000;
-const DANCING_CAT_URL =
-  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const DANCING_CAT_URL = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    img.style.left = `${startPos}px`;
+    img.style.position = 'absolute';
+    let currentPos = startPos;
+
+    const intervalId = setInterval(() => {
+      currentPos += STEP_SIZE_PX;
+      img.style.left = `${currentPos}px`;
+
+      if (currentPos >= stopPos) {
+        clearInterval(intervalId);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    const originalSrc = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = originalSrc;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -24,7 +41,12 @@ async function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  while (true) {
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos, stopPos);
+    img.style.left = `${startPos}px`; // Reset position to start
+  }
 }
 
 window.addEventListener('load', catWalk);
